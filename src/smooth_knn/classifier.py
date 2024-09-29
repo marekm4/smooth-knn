a349d48ccdbd410a60e97684fd0f07858b7ae7b4
Marek Michalik <marekmichalik43@gmail.com>
2024-09-30 01:26:16 +0200

    Switch to using KDTree

--- src/smooth_knn/classifier.py
+++ src/smooth_knn/classifier.py
@@ -1,34 +1,31 @@
 import math
 
 import numpy as np
+from sklearn.neighbors import KDTree
 
 
 class SmoothKNeighborsClassifier:
     def __init__(self, n_neighbors=20, gamma=0.9):
         self.n_neighbors = n_neighbors
         self.gamma = gamma
-        self.X = None
+        self.tree = None
         self.y = None
         self.n_classes = None
 
     def fit(self, X, y):
-        self.X = np.array(X)
+        self.tree = KDTree(np.array(X))
         self.y = np.array(y)
-
-    def distance(self, A, B):
-        return math.sqrt(math.pow(A[0] - B[0], 2) + math.pow(A[1] - B[1], 2))
 
     def predict(self, X):
         predictions = []
-        for x in X:
-            distances = np.array([self.distance(x, d) for d in self.X])
-            closest = distances.argsort()[: self.n_neighbors]
+        distances, closest = self.tree.query(np.array(X), k=self.n_neighbors)
+        for x in range(len(X)):
             results = {}
-            for c in closest:
+            for i, c in enumerate(closest[x]):
                 value = self.y[c]
                 if value not in results:
                     results[value] = []
-                results[value].append(distances[c])
+                results[value].append(distances[x][i])
             for key, value in results.items():
                 results[key] = np.mean(value) * math.pow(self.gamma, len(value))
             predictions.append(min(results, key=results.get))
